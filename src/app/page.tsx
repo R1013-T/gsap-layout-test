b1019b03c6e3b6000afc5081d839d7dc4c6c3104
R1013-T <nosuke1013@icloud.com>
2024-04-30 16:35:38 +0900

    style: init color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
-import Image from 'next/image'
-
 export default function Home() {
-  return <main>TCA ECO SCHOOL FESTIVAL 2024</main>
+  return (
+    <main className="flex flex-col items-center justify-center gap-6 p-10">
+      <p>TCA ECO SCHOOL FESTIVAL 2024</p>
+      <div className="rounded-2xl border bg-card p-10">
+        <p>card</p>
+      </div>
+      <div className="rounded-2xl border bg-primary p-10 text-primary-foreground">
+        <p>card</p>
+      </div>
+      <div className="rounded-2xl border bg-secondary p-10 text-secondary-foreground">
+        <p>card</p>
+      </div>
+      <div className="rounded-2xl border bg-muted p-10 text-muted-foreground">
+        <p>card</p>
+      </div>
+      <div className="rounded-2xl border bg-accent p-10 text-accent-foreground">
+        <p>card</p>
+      </div>
+      <div className="rounded-2xl border bg-destructive p-10 text-destructive-foreground">
+        <p>card</p>
+      </div>
+    </main>
+  )
 }
